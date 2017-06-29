bdfc7308a5b09b726e02f1ac2088d52903a64a74
sachikulkarni <sachikulkarni123@gmail.com>
2017-06-28 19:41:05 -0700

    adding files

---
+++ Interface/Animaltest.java
@@ -0,0 +1,9 @@
+public class Animaltest
+{
+  public static void main(String[] args)
+  {
+    Animal[] zoo=new Animal[3];
+    zoo[0]= new Dog();
+    zoo[1]= new Dog();
+  }
+}
---
+++ Interface/Dog.java
@@ -0,0 +1,11 @@
+public class Dog implements Animal
+{
+  public void eat()
+  {
+    System.out.println("I'm eating");
+  }
+  public void run(int speed)
+  {
+      System.out.println("I'm running at "+speed);
+  }
+}
